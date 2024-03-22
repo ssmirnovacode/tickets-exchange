5b8d90cfa4ba54e972ba5266f35e22bf67c2f006
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-22 15:48:18 +0100

    BadRequestError class added, signup same meail error handled

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { BadRequestError, RequestValidationError } from "../errors";
+import { User } from "../models/user";
 
 const router = express.Router();
 
@@ -14,16 +14,27 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must have between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    console.log("Creating a user...");
 
-    throw new DatabaseConnectionError();
-    res.send({});
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      throw new BadRequestError("Email already in use");
+    }
+
+    // TODO password hashing
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    res.status(201).send(user);
+
+    // TODO send back a cookie or JWT
   }
 );
 
