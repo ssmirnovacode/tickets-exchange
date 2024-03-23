7bb131be0b6d7283aecb998327a7e81415ee60d6
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-23 13:53:36 +0100

    /currentUser route enabled

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.send("hi there current user");
+  // checking if the session cookie jwt is present
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  // validating JWT
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
