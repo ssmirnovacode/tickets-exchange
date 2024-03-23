af90091bd0ddcf6937da5eb8ebeb399689b2e15d
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-23 17:04:43 +0100

    index.ts split to index and app.ts to work with tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,37 +1,5 @@
-import express from "express";
-import "express-async-errors";
-import { json } from "body-parser";
 import mongoose from "mongoose";
-import {
-  currentUserRouter,
-  signinRouter,
-  signoutRouter,
-  signupRouter,
-} from "./routes";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors";
-import cookieSession from "cookie-session";
-
-const app = express();
-app.set("trust proxy", true); // making express trust proxy traffic from ngnix as secure
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false, // disabled encryption
-    secure: true, // https only connection
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all("*", async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
