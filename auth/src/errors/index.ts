3658819ae6d3854e7f1ab7401a962f51f6ec80af
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-23 14:23:51 +0100

    not-authorized error class added, require-auth middleware added

--- auth/src/errors/index.ts
+++ auth/src/errors/index.ts
@@ -3,3 +3,4 @@ export * from "./database-connection-error";
 export * from "./not-found-error";
 export * from "./request-validation-error";
 export * from "./bad-request-error";
+export * from "./not-authorized-error";
