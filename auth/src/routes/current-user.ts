ec7591299fa9191caf7d74dd7769a3513c527fb9
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-23 14:16:52 +0100

    currentUser middleware added

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,21 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import { currentUser } from "../middlewares";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  // checking if the session cookie jwt is present
-  if (!req.session?.jwt) {
-    return res.send({ currentUser: null });
-  }
-
-  // validating JWT
-  try {
-    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
-    res.send({ currentUser: payload });
-  } catch (err) {
-    res.send({ currentUser: null });
-  }
+router.get("/api/users/currentuser", currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
 });
 
 export { router as currentUserRouter };
