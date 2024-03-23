e4e70f9198d87b86665a266603cb5a6d9360fed1
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-23 13:58:08 +0100

    signout route enabled

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-  res.send("hi there signout");
+  req.session = null; // removing the cookie
+  res.send({});
 });
 
 export { router as signoutRouter };
