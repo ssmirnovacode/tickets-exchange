3eacd79ac5ef656fc5b36f2bedf3f48e1e2749a3
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-25 11:36:44 +0100

    global signin() helper function created to be used in all services

--- auth/src/routes/__test__/currentUser.test.ts
+++ auth/src/routes/__test__/currentUser.test.ts
@@ -2,16 +2,10 @@ import request from "supertest";
 import { app } from "../../app";
 
 const EMAIL = "test@test.es";
-const PASSWORD = "password";
 
 describe("/api/users/currentuser", () => {
   it("responds with current user data", async () => {
-    const signupResponse = await request(app)
-      .post("/api/users/signup")
-      .send({ email: EMAIL, password: PASSWORD })
-      .expect(201);
-
-    const cookie = signupResponse.get("Set-Cookie")!;
+    const cookie = await global.signin();
 
     const response = await request(app)
       .get("/api/users/currentuser")
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../app";
+
+const EMAIL = "test@test.es";
+const PASSWORD = "password";
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: MongoMemoryServer;
 
@@ -26,3 +34,17 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+// declaring a global just for convenience
+//NB! it will only be available in test environment
+global.signin = async () => {
+  const email = EMAIL;
+  const password = PASSWORD;
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie")!;
+  return cookie;
+};
