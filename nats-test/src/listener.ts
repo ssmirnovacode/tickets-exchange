b01a6778b7b4db62f7533e8149b790baeec17e8f
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-27 12:30:39 +0100

    Queue group and manualAck added to listener

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,28 @@
 import nats, { Message } from "node-nats-streaming";
+import { randomBytes } from "crypto";
 
 console.clear();
-const stan = nats.connect("ticketing", "123", {
+
+const stan = nats.connect("ticketing", randomBytes(4).toString("hex"), {
   url: "http://localhost:4222",
 });
 
 stan.on("connect", () => {
   console.log("listener connected to NATS");
 
-  const subscription = stan.subscribe("ticket:created");
+  const options = stan.subscriptionOptions().setManualAckMode(true); // we use chaining to configure options
+
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "orders-service-queue-group",
+    options
+  );
   // subscription is listening to a message event:
   subscription.on("message", (msg: Message) => {
-    console.log("ticket:created message received");
+    const data = msg.getData();
+    if (typeof data === "string") {
+      console.log(`Received event #${msg.getSequence()}, with data: ${data}`);
+    }
+    msg.ack();
   });
 });
