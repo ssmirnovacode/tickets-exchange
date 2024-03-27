dbfea2ec984c74a63c8d78eadb9062f7cb6c9f91
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-27 16:51:21 +0100

    Type checking added to custom listeners logic

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,10 +1,12 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./base-listener";
+import { TicketCreatedEvent } from "./ticket-created-event";
+import { Subjects } from "./subjects";
 
-export class TicketCreatedListener extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated; // we add readonly so TS knows we won't change the value
   queueGroupName = "payments-service";
-  onMessage(data: any, msg: Message): void {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message): void {
     console.log("event data", data);
 
     msg.ack();
