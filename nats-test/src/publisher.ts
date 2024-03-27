6f181eda35df85198645ede3b9ce6b2cac2d3c97
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-27 17:14:52 +0100

    Publisher classes added

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,23 +1,19 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 
 console.clear();
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-// instead of async/await we have event-based approach here
-// we listen to the event 'connect' - when client is successfully connected
 stan.on("connect", () => {
   console.log("Publisher connected to NATS");
 
-  // only raw data can be passed, no objects
-  const data = JSON.stringify({
-    id: "123",
+  const publisher = new TicketCreatedPublisher(stan);
+
+  publisher.publish({
+    id: "12312",
     title: "concert",
     price: 20,
-  });
-
-  stan.publish("ticket:created", data, () => {
-    console.log("ticket:created event published");
   });
 });
