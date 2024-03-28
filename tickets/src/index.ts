60662e1dc8040968f967ebfa07f81e914270a780
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-03-28 13:25:16 +0100

    env variables added for NATS in ticketing

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,8 +11,24 @@ const start = async () => {
     throw new Error("MONGO_URI env var must be defined");
   }
 
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID env var must be defined");
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID env var must be defined");
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL env var must be defined");
+  }
+
   try {
-    await natsWrapper.connect("ticketing", "158", "http://nats-srv:4222");
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
     // graceful shut down
     natsWrapper.client.on("close", () => {
       console.log("NATS connection closed");
