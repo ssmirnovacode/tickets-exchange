ba6be0902e2a0f1d2c87d379fcee0ee72e87d210
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-04-01 15:51:28 +0200

    createOrder route validation added, order model drafted

--- orders/src/routes/createOrder.ts
+++ orders/src/routes/createOrder.ts
@@ -3,11 +3,24 @@ import { requireAuth, validateRequest } from "@ticketsx/common";
 import { body } from "express-validator";
 import { natsWrapper } from "../nats-wrapper";
 import { baseUrl } from "../constants";
+import mongoose from "mongoose";
 
 const router = Router();
 
-router.post(baseUrl, async (rew: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  baseUrl,
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("TiketId must be provided and be a valid Mongo ID"),
+  ],
+  validateRequest,
+  async (rew: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as createOrderRouter };
