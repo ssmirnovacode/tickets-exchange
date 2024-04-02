c3ac7b58dacbe8a5bc4a56f4f0b981a64d9817af
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-04-02 15:43:16 +0200

    tests added for createOrder

---
+++ orders/src/routes/__test__/createOrder.test.ts
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { baseUrl } from "../../constants";
+import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@ticketsx/common";
+
+describe("create order", () => {
+  it("returns 401 if not authenticated", async () => {
+    const ticketId = new mongoose.Types.ObjectId();
+
+    await request(app).post(baseUrl).send({ ticketId }).expect(401);
+  });
+
+  it("returns 400 if passed invalid params", async () => {
+    await request(app)
+      .post(baseUrl)
+      .set("Cookie", global.signin())
+      .send({})
+      .expect(400);
+  });
+
+  it("returns an error if the ticket does not exist", async () => {
+    const ticketId = new mongoose.Types.ObjectId();
+
+    await request(app)
+      .post(baseUrl)
+      .set("Cookie", global.signin())
+      .send({ ticketId })
+      .expect(404);
+  });
+
+  it("returns an error if the ticket is already reserved", async () => {
+    const ticket = Ticket.build({
+      title: "concert",
+      price: 23,
+    });
+    await ticket.save();
+
+    const order = Order.build({
+      ticket,
+      userId: "abc",
+      status: OrderStatus.Created,
+      expiresAt: new Date(),
+    });
+    await order.save();
+
+    await request(app)
+      .post(baseUrl)
+      .set("Cookie", global.signin())
+      .send({ ticketId: ticket.id })
+      .expect(400);
+  });
+
+  it("reserves the ticket", async () => {
+    const ticket = Ticket.build({
+      title: "concert",
+      price: 25,
+    });
+    await ticket.save();
+
+    await request(app)
+      .post(baseUrl)
+      .set("Cookie", global.signin())
+      .send({ ticketId: ticket.id })
+      .expect(201);
+  });
+});
