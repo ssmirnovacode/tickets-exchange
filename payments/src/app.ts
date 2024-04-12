0b63042615f9f1e9b1323db006c43bfa9103969e
ssmirnovacode <svetlana.smirnova213@gmail.com>
2024-04-12 15:47:12 +0200

    payments initial setup

---
+++ payments/src/app.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+
+import { errorHandler, NotFoundError, currentUser } from "@ticketsx/common";
+import cookieSession from "cookie-session";
+
+const app = express();
+app.set("trust proxy", true); // making express trust proxy traffic from ngnix as secure
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disabled encryption
+    secure: process.env.NODE_ENV !== "test", // https only connection unless it's jest environment
+  })
+);
+
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,63 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+const EMAIL = "test@test.es";
+
+declare global {
+  var signin: () => string[];
+}
+
+jest.mock("../nats-wrapper");
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  process.env.JWT_key = "whatever"; // we do this to mock env variables that isnt accessible to jest
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = mongo.getUri();
+
+  await mongoose.connect(mongoUri, {});
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  if (mongo) {
+    await mongo.stop();
+  }
+  await mongoose.connection.close();
+});
+
+// declaring a global just for convenience
+//NB! it will only be available in test environment
+global.signin = () => {
+  // build a JWT payload { id, email }
+  const payload = {
+    // we will randomly generate the id to pretend we are a different user on each signup:
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: EMAIL,
+  };
+
+  // create a JWT token
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  // build session object { jwt: token }
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+
+  // take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  // add session= string upfront to match the cookie pattern and return it
+
+  return [`session=${base64}`];
+};
